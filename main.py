6d24414a897f4e3adebe2d467434fe1ea0532eab
Aida <aida.hg0483@gmail.com>
2026-06-26 10:16:09 +0330

    Add show student feacher also add sorting comments for content

--- main.py
+++ main.py
@@ -9,20 +9,31 @@ while user_selection != 5 :
     print("4- Delete Student")
     print("5- Exit")
 
+    # Input validation
     try :
         user_selection = int(input("Enter your selection : "))
     except ValueError :
         print("Invalid Input!\n")
         continue
-
+    
+    # Add student
     if user_selection == 1 :
         student_name = input("Enter Name :")
         student_age = int(input("Enter Age :"))
         student_grade = float(input("Enter Grade :"))
         student = {
-            "Name" : student_name,
-            "Age" : student_age,
-            "Grade" : student_grade
+            "name" : student_name,
+            "age" : student_age,
+            "grade" : student_grade
         }
         students.append(student)
         print("Student's Information Added!")
+
+    # Show student
+    elif user_selection == 2 :
+        if len(students) == 0 :
+            print("No student found!")
+        else :
+            print("\nstudents list :")
+            for index, info in enumerate(students) :
+                print(f"{index + 1}. {info['name']} | age: {info['age']} | grade: {info['grade']}")
